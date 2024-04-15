4d33d99b2dd451306e67c647065914aae4160237
coffemakingtoaster <mh340@hdm-stuttgart.de>
2024-04-15 21:59:51 +0200

    feat(weapons): add throwable weapons

---
+++ Assets/Scripts/ThrowWeapon.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ThrowWeapon : Weapon
+{
+
+    void setupAudioSource(){
+        if (audioSource != null){
+            return;
+        }
+        audioSource = FirePoint.GetComponent<AudioSource>();
+        if (audioSource == null){
+            audioSource = FirePoint.gameObject.AddComponent<AudioSource>();
+        }
+        audioSource.clip = WeaponSound;
+    }
+    
+    public override void shoot()
+    {
+        if(currentAmmo > 0){
+            setupAudioSource();
+            audioSource.Play();
+            changeAmmo(-1);
+            // model.GetComponent<Animator>().Play("Armature|Shoot");
+
+            model.transform.parent = null;
+            model.GetComponent<ThrowableBehaviour>().yeet(playerCam.transform.TransformDirection(Vector3.forward).normalized);
+        }
+    }
+
+    public override void secondary()
+    {
+        base.secondary();
+    }
+
+}
---
+++ Assets/Scripts/ThrowableBehaviour.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class ThrowableBehaviour : MonoBehaviour
+{
+
+    public float speed;
+
+    private Vector3 direction;
+    
+    public float MaxLifeTime = 10f;
+
+    private GameMaster gameMaster;
+    
+
+    public GameObject DecalParticles;
+
+    private bool hasBeenThrown = false;
+
+    public float Damage = 5;
+
+    public void Start(){
+        gameMaster = GameObject.Find("GameMaster").GetComponent<GameMaster>();
+        GetComponent<BoxCollider>().enabled = false;
+    }
+
+    public void yeet(Vector3 direction){
+        GetComponent<BoxCollider>().enabled = true;
+        Rigidbody rigidbody =  gameObject.AddComponent<Rigidbody>(); 
+        rigidbody.isKinematic = false;
+        rigidbody.mass = 0.1f;
+        rigidbody.AddForce(direction.normalized * speed, ForceMode.Impulse);
+        hasBeenThrown = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasBeenThrown){
+            return;
+        }
+        MaxLifeTime -= Time.deltaTime;
+        if (MaxLifeTime <= 0){
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision){
+        if (!hasBeenThrown){
+            return;
+        }
+        if (collision.gameObject.name == "Player"){
+            return;
+        }
+
+        GameObject c = Instantiate(DecalParticles, transform.position, Quaternion.identity);
+        Destroy(c, 1);
+
+        if (collision.gameObject.tag == "Boss"){
+                BossBehaviour boss = FindObjectOfType<BossBehaviour>();
+
+                if(boss != null){
+                    boss.Damage(Damage);
+                }
+        }
+
+        Destroy(gameObject);
+    }
+}
